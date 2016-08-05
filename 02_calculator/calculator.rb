5ca7c2529e41600b655143960b8331448caa44c2
Chris Davis <chris.davis5440@gmail.com>
2016-08-05 13:12:24 -0400

    Upddated Calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,15 @@
-#write your code here
+def add(a,b)
+	a+b
+end
+
+def subtract(a,b)
+	a-b
+end
+
+def sum(num)
+ num.inject(0) {|sum, i|  sum + i }
+end
+
+def multiply(*nums)
+ nums.inject(:*)
+end
